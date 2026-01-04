27f36a003d0f9b427c0a3a3a809dd6e353491a47
snavohburmaa <khantwaiyanaung1335@gmail.com>
2026-01-04 10:33:53 +0700

    Update FalseInvariantsTest.t.sol

--- test/fuzz/FalseInvariantsTest.t.sol
+++ test/fuzz/FalseInvariantsTest.t.sol
@@ -5,14 +5,14 @@
 
 // //fail_on_revert = false
 
-//pragma solidity ^0.8.19;
+pragma solidity ^0.8.19;
 
-// import {Test} from "forge-std/Test.sol";
-// import {DeployDSC} from "../../script/DeployDSC.s.sol";
-// import {DSCEngine} from "../../src/DSCEngine.sol";
-// import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
-// import {HelperConfig} from "../../script/HelperConfig.s.sol";
-// import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 // contract InvariantsTest is Test {
 //     DeployDSC deployer;
